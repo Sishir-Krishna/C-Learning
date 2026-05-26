866834bd4e74054608a4dad9f61acf9cc57e6f2e
B Sishir Krishna <krishnagames7@gmail.com>
2026-05-26 20:05:49 +0530

    Trial Codes run

---
+++ NPTEL/2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("Welcome to ");
+    printf("C Programming");
+}
--- filler.c
+++
@@ -1,6 +0,0 @@
-# include <stdio.h>
-int main(void)
-{
-    printf("I Couldn't get around to starting C today due to medical problems, but I will start tomorrow. I promise.\n");
-    return 0;
-}

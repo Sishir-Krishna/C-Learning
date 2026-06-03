b40d80bec042fc57080aba5a3d70f5a2a97e9141
B Sishir Krishna <krishnagames7@gmail.com>
2026-06-03 18:11:57 +0530

    v2.0

--- Let Us C/Chapter 8/prime_factor_function.c
+++ Let Us C/Chapter 8/prime_factor_function.c
@@ -1,51 +1,50 @@
 #include <stdio.h>
-char prime_or_not(int);
+int prime_or_not(int);
 int main()
 {
     int num,factor;
     printf("Enter a positive integer: \n");
     scanf("%d", &num);
+    printf("The prime factors of %d are: \n", num);
     for (int i=2;i<num;i++)
     {
         if (num%i == 0)
         // Implies that i is a factor of the number. Now we have to check if i is prime or not.
         {
-            char result_of_prime_or_not = prime_or_not (i);
-            if (result_of_prime_or_not == 'y')
+            int result_of_prime_or_not = prime_or_not (i);
+            if (result_of_prime_or_not == i)
             {
-                printf("The prime factors of %d are: \n", num);
-                for (int j=i; (num/j) != i; num = (num/j))
+                for (int j=num; j%i == 0; j= j/i)
                 {
-                    printf("%d", i);
+                    printf("%d ", i);
                 }
             }
-            else if (result_of_prime_or_not == 'n')
+            else if (result_of_prime_or_not != i)
             continue ;
             // i is a factor, but not a prime factor of the number
             // go for the next i value.
         }
     }
 }
-char prime_or_not(int x)
+int prime_or_not(int x)
 {
     int j=2;
-    char check;
-    for (j=2;j<x;j++)
+    if (x == 2)
+    return (x);
+    else
     {
-        if (x%j != 0)
+        for (j=2;j<x;j++)
         {
-            if (j<(x-1))
-            continue;
-            else if (j==(x-1))
+            if (x%j != 0)
             {
-                check = 'y';
-                return check;
+                continue ;
             }
+            else
+            break;
         }
-        else if (x%j == 0)
-        {
-            check = 'n';
-            return check;
-        }
+        if (j==x)
+        return (x);
+        else
+        return 0;
     }
 }

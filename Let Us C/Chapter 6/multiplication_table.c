d29afaa1872cc269f28ce12d4e378e21aabd0216
B Sishir Krishna <krishnagames7@gmail.com>
2026-08-01 13:33:00 +0530

    logic error fixed

--- Let Us C/Chapter 6/multiplication_table.c
+++ Let Us C/Chapter 6/multiplication_table.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int num,i,product;
+    int num,i,product,table_end;
     printf("Enter a number: \n");
     scanf("%d", &num);
+    printf("Enter the end of the table: \n");
+    scanf("%d", &table_end);
     printf("The multiplication table of %d is: \n", num);
-    for (i=1;;i++)
+    for (i=1;i<=table_end;i++)
     {
         product = num*i;
         printf("%d * %d = %d\n",num, i, product);
-        continue;
     }
     return 0;
 }

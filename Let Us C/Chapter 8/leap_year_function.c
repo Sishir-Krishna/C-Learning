d564ea8542e2336d1cc7ee57b8d04305ecc88f3e
B Sishir Krishna <krishnagames7@gmail.com>
2026-08-06 00:46:08 +0530

    better logic

--- Let Us C/Chapter 8/leap_year_function.c
+++ Let Us C/Chapter 8/leap_year_function.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
-char leap_year(int x);
+void leap_year (int x);
 int main()
 {
     int year;
-    printf("Enter an year: \n");
+    printf("Enter any year: \n");
     scanf("%d", &year);
-    char result = leap_year(year);
-    if (result == 'y')
-    printf("The year %d is a leap year.\n", year);
-    else if (result == 'n')
-    printf("The year %d is not a leap year.\n", year); 
+    leap_year(year);
     return 0;
 }
-char leap_year(int year)
+void leap_year (int x)
 {
-    char check;
-    if (year%4 != 0)
-    check = 'n';
-    else if (year%4 ==0)
+    if (x%4==0 && x%100!=0 || x%400==0)
     {
-        if (year%100 == 0)
-        {
-            if (year%400 == 0)
-            check = 'y';
-            else
-            check = 'n';
-        }
-        else
-        check = 'y';
+        printf("%d is a leap year", x);
     }
-    return check;
+    else
+    {
+        printf("%d is not a leap year", x);
+    }
 }
